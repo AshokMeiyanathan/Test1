50c45df9a7f6f29edef11b3fba242d7c33237ce9
AshokMeiyanathan <51266778+AshokMeiyanathan@users.noreply.github.com>
2019-06-01 23:39:53 -0400

    Added files via upload
    Added files via upload for testing

---
+++ githubtesting/Xpath1.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package xpath;
+
+/**
+ * @author AMeiyanathan
+ *
+ */
+public class Xpath1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// We should use relative xpath only. We should not use absoluate xpath
+/*Already we have seen the below link
+https://www.youtube.com/watch?v=As72m9qrAQ8
+*/	}
+
+}
+
+/*Understnd ancestor sibling precence follower*/
